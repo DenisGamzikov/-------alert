50f56a374a2564d7c90c7769390ee9a68b167d47
Denis <gamzikovd@yandex.ru>
2023-08-01 10:24:31 +0300

    last-part

--- script.js
+++ script.js
@@ -20,4 +20,101 @@ SecondNumber = Number(SecondNumber)
 let sum = firstNumber + SecondNumber;
 alert("сумма чисел = " + sum)
 
+// задание на отсчет
+let Countdown = prompt("Введите любое число от 0 до 10")
+Countdown = Number(Countdown)
+if(Countdown > 10) {
+     Countdown = 10
+}
+alert(Countdown)
+
+  
+  while (Countdown > 0 ) {
+    Countdown = Countdown - 1
+       alert(Countdown)
+     }
+
+
+
+//   Задание на звуки животных
+  let animal = prompt("Введите свое любимое животное")
+  switch (animal) {
+    case 'собака': { 
+        alert("Гав-гав")
+      break;	
+    }
+    case 'кошка': {
+        alert("Мяу-мяу")
+      break;	
+    }
+    case 'корова': {
+        alert("Муу")
+      break;	
+    }
+    case 'петух': {
+    alert("Кукареку")
+    break;	
+  }
+    default: {
+        alert("Мы не знаем такого животного")
+    }
+  }
+  
+
+// Задание на математический пример
+  let firstNumeric = prompt("Введите первое число")
+  firstNumeric = Number(firstNumeric)
+  let SecondNumeric = prompt("Ведите второе число");
+  SecondNumeric = Number(SecondNumeric)
+  let sign = prompt("Введите арифметичческий знак(+,-,*,/)")
+  
+  
+  switch(sign) {
+      case "+":
+      result = firstNumeric + SecondNumeric
+      alert(result)
+      break
+      case "-":
+      result = firstNumeric - SecondNumeric
+      alert(result)
+      break
+      case "*":
+      result = firstNumeric * SecondNumeric
+      alert(result)
+      break
+      case "/":
+      result = firstNumeric / SecondNumeric
+      alert(result)
+      break
+  }
+  const UserAgreed = confirm("Дайте свое согласие");
+     while (UserAgreed == false) {
+      confirm("Дайте свое согласие")
+      break
+      } 
+    
+
+
+ 
+
+//   задание на вопрос возраста и пола
+let Age = prompt("Введите свой возраст")
+Age = Number(Age)
+let Gender = prompt("Вы мужского пола?")
+if (Age >= 16 && Gender == "Да") {
+    alert("Нам кажется, вам необходимы интструменты")
+}
+else if (Age < 16 && Gender == "Да") {
+    alert("Нам кажется, вам необходимы машинки")
+}
+else if (Age > 16 && Gender == "Нет") {
+    alert("Нам кажется, вам необходимы сумки")
+}
+else if (Age < 16 && Gender == "Нет") {
+    alert("Нам кажется, вам необходимы Куклы")
+}
+else {
+    alert("Вы неправильно ответили на какой-то из вопросов")
+}
+
 
