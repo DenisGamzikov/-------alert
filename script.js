9c7bb4f6a66211faf9ddde1ddbd9d4b76b0b086d
Denis <gamzikovd@yandex.ru>
2023-08-01 06:33:29 +0300

    first-part

---
+++ script.js
@@ -0,0 +1,23 @@
+// Задание с разными типами данных, и выведение имени на экран
+alert("Привет")
+alert("12")
+let any
+alert(any)
+alert("null")
+let check = 10 > 3
+alert(check)
+let userName = prompt("Как тебя зовут?");
+
+
+alert("Добро пожаловать на сайт, " + userName)
+
+
+// задание на сумму двух чисел
+let firstNumber = prompt("Введите первое число")
+firstNumber = Number(firstNumber)
+let SecondNumber = prompt("Ведите второе число");
+SecondNumber = Number(SecondNumber)
+let sum = firstNumber + SecondNumber;
+alert("сумма чисел = " + sum)
+
+
